bfdb3ef142fea2668aa7c0670b66f948cc55145a
wuchao <wuchao@haima.me>
2017-10-13 16:46:59 +0800

    修改url编码方法

--- src/utils/WeChat.js
+++ src/utils/WeChat.js
@@ -62,7 +62,7 @@ export default class WeChat {
     wx.onMenuShareTimeline({
       title: document.title, // 分享标题
       desc: desc,
-      link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      link: encodeURI(window.location.href.split('#')[0]), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: icon, // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
@@ -83,7 +83,7 @@ export default class WeChat {
     wx.onMenuShareAppMessage({
       title: document.title, // 分享标题
       desc: desc, // 分享描述
-      link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      link: encodeURI(window.location.href.split('#')[0]), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: icon, // 分享图标
       type: 'link', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -104,7 +104,7 @@ export default class WeChat {
     wx.onMenuShareQQ({
       title: document.title, // 分享标题
       desc: desc, // 分享描述
-      link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接
+      link: encodeURI(window.location.href.split('#')[0]), // 分享链接
       imgUrl: icon, // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
@@ -123,7 +123,7 @@ export default class WeChat {
     wx.onMenuShareQZone({
       title: document.title, // 分享标题
       desc: desc, // 分享描述
-      link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接
+      link: encodeURI(window.location.href.split('#')[0]), // 分享链接
       imgUrl: icon, // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
@@ -142,7 +142,7 @@ export default class WeChat {
     wx.onMenuShareWeibo({
       title: document.title, // 分享标题
       desc: desc, // 分享描述
-      link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接
+      link: encodeURI(window.location.href.split('#')[0]), // 分享链接
       imgUrl: icon, // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
