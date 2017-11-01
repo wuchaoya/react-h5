cd6645038a5ce9491e11c33c19e9dc57ef4da05e
wuchao <wuchao@haima.me>
2017-11-01 11:24:44 +0800

    增加活动页

--- src/components/ActFours.js
+++ src/components/ActFours.js
@@ -1,3 +1,88 @@
 /**
  * Created by chao on 2017/11/1.
  */
+import React, { Component } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay, virtualize, bindKeyboard } from 'react-swipeable-views-utils';
+
+import banner1 from '../assets/banner-庞然巨物.jpg';
+import banner2 from '../assets/banner-我的宠物Pou.jpg';
+import banner3 from '../assets/banner-逃离实验室.jpg';
+import banner4 from '../assets/banner-飞翔彩虹猫.jpg';
+
+const EnhancedSwipeableViews = bindKeyboard(autoPlay(virtualize(SwipeableViews)));
+const data = [banner1, banner2, banner3, banner4];
+
+export default class ActFours extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      index:0
+    };
+    this.slideRenderer = this.slideRenderer.bind(this);
+  }
+  slideRenderer (params) {
+    const { index, key } = params;
+    return (
+      <img
+        alt=''
+        src={
+          // eslint-disable-next-line
+          data[index]
+        }
+        style={styles.slide} key={key} />
+    );
+  }
+  indexRender (data) {
+    return (
+      <div style={styles.spanList}>
+        {data.map((item, index) => {
+          return <span key={'span_' + index} style={
+            Object.assign({}, styles.spanStyle, this.state.index === index ? styles.selectStyle : {})
+          } />;
+        })}
+      </div>
+    );
+  }
+  render () {
+    return (
+      <div style={styles.container}>
+        <EnhancedSwipeableViews onChangeIndex={
+          (index, indexLatest) => {
+            this.setState({
+              index: index
+            });
+          }
+        } slideCount={data.length} slideRenderer={this.slideRenderer} />
+        {this.indexRender(data)}
+      </div>
+    );
+  }
+}
+const styles = {
+  container: {
+    position: 'relative'
+  },
+  slide: {
+    width: '7.2rem',
+    height: '4.06rem'
+  },
+  spanStyle: {
+    display:'inline-block',
+    width: '0.18rem',
+    height: '0.18rem',
+    borderRadius:'50%',
+    backgroundColor:'#fff',
+    marginLeft:'0.1rem'
+  },
+  spanList: {
+    position: 'absolute',
+    right: '0.3rem',
+    bottom:'0.18rem'
+  },
+  selectStyle: {
+    backgroundColor:'#fc9100',
+    opacity:'1'
+  }
+};
+
--- src/containers/Activity.js
+++ src/containers/Activity.js
@@ -3,38 +3,89 @@
  */
 
 import React, { Component } from 'react';
+import ActFours from '../components/ActFours';
 
 export default class Activity extends Component {
   render () {
     return (
       <div>
-        <div />
-        <div>
-          <div>
+        <ActFours />
+        <div style={styles.textContainer}>
+          <div style={styles.line}>
             <h2 style={styles.title}>产品规则:</h2>
             <p style={styles.text}>每个用户同个周期(5天内)只能办理一次，不能重复办理。</p>
           </div>
           <div>
             <h2 style={styles.title}>权益:</h2>
-            <p style={styles.text}>1</p>
+            <p style={styles.text}>1、订购用户享受20GB省内流量(有效期5天)</p>
+            <p style={styles.text}>2、订购即送咪咕斗地主5000乐豆、100张兑换券、10张参赛券尊享礼包。
+              (备注：订购后页面领取，权益即刻到账；非页面领取10个工作日内到账，道具有效期以游戏内为准。)
+            </p>
+            <p style={styles.text}>3、订购用户从订购之日算起，通过权益领取页面或者其他方式登陆进入
+              咪咕云游戏H5页面，享受4个月云游戏不限时长.
+            </p>
           </div>
         </div>
-        <div />
+        <div style={styles.center}>
+          <div style={styles.button}>
+            我要办理
+          </div>
+        </div>
       </div>
     );
+  }
+  componentWillMount () {
+    document.getElementsByTagName('html')[0].style.background = '#fff';
+    document.getElementsByTagName('body')[0].style.background = '#fff';
   }
 };
 
 const styles = {
+  textContainer: {
+    paddingRight:'0.48rem',
+    paddingLeft:'0.48rem'
+  },
   title: {
     fontSize:'0.26rem',
     color:'#fd7303',
     fontWeight:'900',
-    fontFamily: 'Microsoft YaHei'
+    fontFamily: 'Microsoft YaHei',
+    margin: '0',
+    padding:'0',
+    marginTop:'0.4rem',
+    marginBottom:'0.22rem'
   },
   text: {
-    fontSize:'0.24rem',
-    color:'#a85924',
-    fontFamily:'Lantinghei SC'
+    fontSize:'0.26rem',
+    color:'#343434',
+    fontFamily:'Lantinghei SC',
+    fontWeight:'400',
+    lineHeight:'0.4rem',
+    margin:'0',
+    padding:'0',
+    marginBottom:'0.32rem'
+  },
+  button: {
+    width: '4.36rem',
+    height:'0.69rem',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    background:'#fd7303',
+    fontSize:'0.3rem',
+    fontFamily:'Lantinghei SC',
+    color:'#fff',
+    fontWeight:'400',
+    borderRadius:'0.12rem'
+  },
+  center: {
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    height:'0.97rem',
+    borderTop: '0.01rem solid #ededed'
+  },
+  line: {
+    borderBottom:'0.01rem solid #ededed'
   }
 };
